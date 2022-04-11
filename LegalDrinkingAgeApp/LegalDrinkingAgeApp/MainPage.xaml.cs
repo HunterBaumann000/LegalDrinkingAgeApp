4c2b6493331710edb03c980780b742090a21ebbd
HunterBaumann000 <71730976+HunterBaumann000@users.noreply.github.com>
2022-04-11 18:15:17 -0500

    Add project files.

---
+++ LegalDrinkingAgeApp/LegalDrinkingAgeApp/MainPage.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace LegalDrinkingAgeApp
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        void Handle_Clicked(Object sender, System.EventArgs e)
+        {
+            int userAge = Int32.Parse(Age.Text);
+            int legalAge = 21;
+
+            if(userAge >= legalAge)
+            {
+                TheLabel.Text = $"You are able to drink, you are {userAge} years old";
+                YearsUntilLabel.Text = "";
+            } else
+            {
+                TheLabel.Text = $"You cannot drink, you are {userAge} years old";
+                YearsUntilLabel.Text = $"You can drink in {legalAge - userAge} year(s)";
+            }
+            
+            
+        }
+    }
+}
